783babbb25ceb34b76282d7a7bb783c0400ace66
Sean Lemmor Espinosa <seanlemmor.espinosa@gmail.com>
2022-08-26 03:34:17 +0800

    Technically my first commit on my local git repository, second commit after the README.md

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        System.out.println("Hello, Sean!");
+
+        String myName = "Sean Espinosa";
+        System.out.println("Hello, " + myName + "!");
+    }
+}
